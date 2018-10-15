275d0884771b6e59bd1b732639e950651e922d9e
DirtbagDrew <andrewmyerhb@gmail.com>
2018-10-15 12:37:57 -0700

    heapster added

--- Prolog/family_tree/family_tree.pl
+++ Prolog/family_tree/family_tree.pl
@@ -13,3 +13,8 @@ female(michelle).
 
 sibling(X, Y) :- parent(Z, X), parent(Z, Y), X \= Y.
 brother(X, Y) :- male(X), sibling(X, Y).
+aunt(X, Y) :-  female(X),parent(Z,Y),sibling(X,Z).
+cousin(X,Y):- parent(Px,X),parent(Py,Y),sibling(Px,Py).
+grandparent(X,Y):-parent(Py,Y),parent(X,Py).
+descendent(X,Y):-parent(Y,X).
+descendent(X,Y):-parent(P,X),descendant(P,Y).
